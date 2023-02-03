59102b0575d61464b48652afa74d5327f76f1f50
A. Immanual Sarker <66303689+immanual19@users.noreply.github.com>
2023-02-03 12:12:36 +0600

    Add files via upload

---
+++ Stack.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    
+    cout<<st.top()<<endl;
+    
+    st.pop();
+    cout<<st.top()<<endl;
+    
+    st.pop();
+    cout<<st.top()<<endl;
+    
+    cout<<st.size()<<endl;
+    
+    return 0;
+}
